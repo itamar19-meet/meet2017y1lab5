10395ae63680105b51bbd713dc32545fac124ff5
itamar19-meet <itamar19@meet.mit.edu>
2017-07-27 12:13:28 +0300

    Stopping here

---
+++ add_numbers.py
@@ -0,0 +1,7 @@
+def add():
+    start = input("what is the start number?")
+    end = input("what is the final number?")
+    for i in range(int(start) , int(end) + 1):
+        print(i)
+    return i 
+
