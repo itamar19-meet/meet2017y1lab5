da47f6fecef24d6f39752acbee0e1a6da846ac12
itamar19-meet <itamar19@meet.mit.edu>
2017-07-30 17:35:38 +0200

    Stopping here

---
+++ stringfun.py
@@ -0,0 +1,15 @@
+def string_tst(length):
+    if len(length) > 2 and length[0] == length[-1]:
+
+        return(True)
+  
+    else:
+        print(False)
+def add_x(length):
+    
+    length + "x"
+    "x" + length    
+    print(length)
+    return(True)
+eecklknjiok
+    
